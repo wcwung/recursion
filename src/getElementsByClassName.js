db9858a317f6bd2d55e553c5ab04fd1c87c1fd24
William Wung <williamcwung@gmail.com>
2015-01-26 05:12:26 -0800

    Finished getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,23 @@
 var getElementsByClassName = function(className
 ){
   // your code here
+  var result = [];
+
+  var getSpecifiedElement = function(element) {
+  	var elementClass = element.classList;
+  	var children = element.childNodes;
+  	
+  	if (elementClass) {
+  		if (elementClass.contains(className)) {
+  			result.push(element);
+  		}
+  	}
+
+  	for (var i = 0; i < children.length; i++) {
+  		getSpecifiedElement(children[i]);
+  	}
+  }
+
+  getSpecifiedElement(document);
+  return result;
 };
